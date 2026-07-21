08f3eb4e3635811aafea986866093cb8e38b4391
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2026-07-21 17:16:38 +0600

    remove sdkman

--- .config/fish/extras/sdkman.fish
+++
@@ -1,9 +0,0 @@
-
-set -l SDKMAN_DIR "$DOT_PLUGIN_HOME/sdkman"
-if test -d "$SDKMAN_DIR"
-    set -gx SDKMAN_DIR "$SDKMAN_DIR"
-    function sdk
-        bash -lc 'source "${SDKMAN_DIR}/bin/sdkman-init.sh" && sdk $argv'
-    end
-end
-
